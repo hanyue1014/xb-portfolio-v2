23ba36e85613ecf256323d126af0de28518d0472
hanyue1014 <wongxuanbin@gmail.com>
2021-12-15 17:51:49 +0800

    Beautified code

--- src/components/Nav/NavLink.js
+++ src/components/Nav/NavLink.js
@@ -4,7 +4,10 @@ import classes from './styles/navlink.module.css'
 
 function NavLink(props) {
   return (
-    <div className={`${classes.navLink} ${props.active ? classes.active : ''}`} onClick={() => props.onClick(props.linksTo)}>
+    <div 
+      className={`${classes.navLink} ${props.active ? classes.active : ''}`} 
+      onClick={() => props.onClick(props.linksTo)}
+    >
       <i className={`icofont-${props.iconName} ${classes.navIcon}`}></i>
       <p>{props.linksTo}</p>
     </div>
